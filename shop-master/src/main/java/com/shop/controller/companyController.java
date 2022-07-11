2a74e9016d7de320c137aeb9c2fa497b0ca681c0
yiaby <yiaby1309@gmail.com>
2022-07-11 14:25:56 +0900

    캐논페이지 DB 연동, 슬라이더

--- shop-master/src/main/java/com/shop/controller/companyController.java
+++ shop-master/src/main/java/com/shop/controller/companyController.java
@@ -1,14 +1,35 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.RequiredArgsConstructor;
+import com.shop.dto.ItemDto;
+import com.shop.service.ItemService;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping(value="/company")
 
+
+
 public class companyController {
+
+    private final ItemService itemService;
+
     @GetMapping(value = "/nikon" )
     public String nikonPage(Model model){
         model.addAttribute("brand","nikon 페이지 입니다.");
@@ -25,8 +46,14 @@ public class companyController {
         return "company/olymPage";}
 
     @GetMapping(value = "/canon" )
-    public String canonPage(Model model){
-        model.addAttribute("brand","canon 페이지 입니다.");
+    public String canonPage(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model){
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
+        Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
+
+
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
         return "company/canonPage";}
 
     @GetMapping(value = "/fujifilm" )
